6f72b301771998798b24c3465a0c968938968f08
Tomasz Miąsko <tomasz.miasko@gmail.com>
2018-12-16 00:00:00 +0000

    Add tests for gobject-sys based on ctest

---
+++ gobject-sys-test/build.rs
@@ -0,0 +1,81 @@
+extern crate ctest;
+extern crate shell_words;
+
+fn pkg_config_cflags() -> Result<Vec<String>, Box<std::error::Error>> {
+    let mut cmd = std::process::Command::new("pkg-config");
+    cmd.arg("--cflags");
+    cmd.arg("gobject-2.0");
+    let out = cmd.output()?;
+    if !out.status.success() {
+        return Err(format!("command {:?} returned {}",
+                           &cmd, out.status).into());
+    }
+    let stdout = std::str::from_utf8(&out.stdout)?;
+    Ok(shell_words::split(stdout.trim())?)
+}
+
+fn main() {
+    let mut cfg = ctest::TestGenerator::new();
+
+    for flag in pkg_config_cflags().unwrap() {
+        cfg.flag(&flag);
+    }
+
+    cfg.header("glib-object.h");
+
+    cfg.skip_const(|_| true);
+    cfg.skip_fn(|_| true);
+    cfg.skip_signededness(|_| true);
+
+    cfg.type_name(|ty, _is_struct, _is_union| {
+        // Use Foo, instead of struct Foo, etc.
+        ty.to_string()
+    });
+
+    cfg.skip_field_type(|typ, field| match (typ, field) {
+        // FIXME in gir: const mismatch:
+        ("GInitiallyUnownedClass", "set_property") => true,
+        ("GObjectClass", "set_property") => true,
+        ("GTypeValueTable", "collect_value") => true,
+
+        // Const:
+        ("GSignalQuery", "param_types") => true,
+
+        // Volatile:
+        ("GInitiallyUnowned", "ref_count") => true,
+        ("GObject", "ref_count") => true,
+        ("GWeakRef", "priv_") => true,
+
+        // Unnamed type:
+        ("GValue", "data") => true,
+
+        _ => false,
+    });
+
+    cfg.field_name(|_typ, field| match field {
+        // Unfix rust keywords in field names.
+        "priv_" => "priv",
+        "type_" => "type",
+        _ => field,
+    }.to_string());
+
+    cfg.skip_struct(|typ| match typ {
+        // Incomplete types, need manual exclusion since ctype
+        // doesn't understand "pub struct Type(c_void)" pattern:
+        "GBinding" => true,
+        "GCClosure" => true,
+        "GClosure" => true,
+        "GParamSpecPool" => true,
+        "GTypeCValue" => true,
+        "GTypePlugin" => true,
+
+        // Unnamed types:
+        "GValue_data" => true,
+        "GWeakRef_priv" => true,
+
+        _ => false,
+    });
+
+    cfg.generate("../gobject-sys/src/lib.rs", "all.rs");
+}
+
